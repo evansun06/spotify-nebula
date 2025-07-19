b89f2d248321c22f1bc83525d24e49113b98de1f
Evan Sun <evansun06@outlook.com>
2025-07-18 22:12:23 -0600

    restructured the project to follow scalable backend development

---
+++ src/models/track.py
@@ -0,0 +1,20 @@
+x   from pydantic import BaseModel
+    from typing import list
+
+class Track_Data(BaseModel):
+    name: str
+    artist: List[str]
+    audio_features: Audio_Features
+
+
+
+class Audio_Features(BaseModel):
+    acousticness: float
+    danceability: float
+    energy: float
+    instrumentalness: float
+    loudness: float
+    tempo: float
+
+    
+
