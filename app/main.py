08d1bb70faa04a30357f16d0c0fb92f67332a24f
lyanriu8 <ryanliu103@gmail.com>
2025-08-10 16:30:28 -0600

    small fixes

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers import auth
-
-from app.database.create_db import engine, Base
-
-server = FastAPI()
-Base.metadata.create_all(bind=engine)
-server.include_router(auth.router)
