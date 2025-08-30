7cdef632bba612cbbfd05f11dbc3005d6bd9ac2e
Evan Sun <evansun06@outlook.com>
2025-08-30 12:44:32 -0700

    created simple threeJS webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
+import Nebula from './pages/nebula/Nebula.jsx'
+import sample from '../sample.json'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('../sample.json')
+      .then(response => response.json())
+      .then(json => {
+        setData(json); // Store the data in state
+      })
+      .catch(err => console.error('Failed to load JSON:', err));
+  }, []);
 
   return (
-    <div>Work in progress</div>
+    <Nebula data={data} /> 
   );
 }
 
-export default App
+export default App;
